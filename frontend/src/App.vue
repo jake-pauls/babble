1712f9c46b4a83a11c1d51f0f3df1c5e979e0178
jacob-pauls <jakepauls99@gmail.com>
2021-06-08 22:41:54 -0700

    Created sample vue app and cleaned out create-vue boilerplate

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+      <img alt="Babble logo" src="./assets/logo.png">
+      <Landing msg="Welcome to Babble"/>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+#app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #0A0A0A;
+  margin-top: 220px;
+}
+</style>
