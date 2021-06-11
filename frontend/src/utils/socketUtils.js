cb846dc0fbab5aab0e140eb0bbfeaa555fdcb97f
bootsie123 <bootsie227@gmail.com>
2021-06-11 16:43:26 -0400

    Used PROD variable instead of VUE environment variable

--- frontend/src/utils/socketUtils.js
+++ frontend/src/utils/socketUtils.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client'
+import { __PROD__ } from './env'
 
 export const socket = io(process.env.VUE_APP_PEER_SERVER_URL || 'localhost', {
-  secure: process.env.VUE_APP_PEER_SERVER_URL ? true : false
+  secure: __PROD__ ? true : false
 })
