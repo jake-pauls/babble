c001ef9db9cf033f65ddbbacd3c82957a7b67636
bootsie123 <bootsie227@gmail.com>
2021-06-11 16:42:30 -0400

    Added support for websockets over HTTPS

--- frontend/src/utils/socketUtils.js
+++ frontend/src/utils/socketUtils.js
@@ -1,3 +1,5 @@
 import io from 'socket.io-client'
 
-export const socket = io(process.env.VUE_APP_PEER_SERVER_URL || 'localhost')
+export const socket = io(process.env.VUE_APP_PEER_SERVER_URL || 'localhost', {
+  secure: process.env.VUE_APP_PEER_SERVER_URL ? true : false
+})
