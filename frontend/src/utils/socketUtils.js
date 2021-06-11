022296cb151d402fa331453716ae41d290eab649
bootsie123 <bootsie227@gmail.com>
2021-06-11 17:18:40 -0400

    Test fix

--- frontend/src/utils/socketUtils.js
+++ frontend/src/utils/socketUtils.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client'
 import { __PROD__ } from './env'
 
-export const socket = io('babble-backend.herokuapp.com' || 'localhost', {
-  secure: __PROD__ ? true : false
+export const socket = io(process.env.VUE_APP_PEER_SERVER_URL || 'localhost', {
+  secure: __PROD__ ? true : false,
+  transports: ['websocket']
 })
