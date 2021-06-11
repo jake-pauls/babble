f5f36abb3010a54d98beb77e1d727217bd82a09d
bootsie123 <bootsie227@gmail.com>
2021-06-11 18:55:48 -0400

    Made polling the default transport for socket.io

--- frontend/src/utils/socketUtils.js
+++ frontend/src/utils/socketUtils.js
@@ -2,5 +2,6 @@ import io from 'socket.io-client'
 import { __PROD__ } from './env'
 
 export const socket = io(process.env.VUE_APP_PEER_SERVER_URL || 'localhost', {
-  secure: __PROD__ ? true : false
+  secure: __PROD__ ? true : false,
+  transports: ['polling']
 })
